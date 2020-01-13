5f043a0f2b4ee872c3b11d53a9d0ba48bdff8d8b
Михаил <shipov-mihail@mail.ru>
2020-01-13 15:45:38 +0300

    Added class "Mines"

---
+++ Mines.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPFirst
+{
+    class Mines
+    {
+        int number;
+        int x;
+        int y;
+        Random randX = new Random();
+        Random randy;
+
+        public Mines (int _number)
+        {
+            for (int i = 0; i < _number; i++)
+            {
+                x = randX.Next(1,48);
+                y = randy
+            }
+        }
+    }
+}
